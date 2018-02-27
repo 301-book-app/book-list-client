fc9dd37b6cee0e43f7666774372d7da4662c9282
Brent Williams <wtnerb@gmail.com>
2018-02-27 11:23:30 -0800

    error template

--- scripts/views/error-view.js
+++ scripts/views/error-view.js
@@ -11,7 +11,7 @@ const app = app || {};
     $('#error-message').empty();
     let template = Handlebars.compile($('#error-template').text());
     $('#error-message').append(template(err));
-  };
+  }; //TODO check error template
 
   module.errorView = errorView;
 })(app);
