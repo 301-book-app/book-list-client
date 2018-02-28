e9141d168a92dd7ee754d40351596f96cd4ac6e6
Brent Williams <wtnerb@gmail.com>
2018-02-28 10:51:21 -0800

    filled out routejs

--- controller/routes.js
+++ controller/routes.js
@@ -1,1 +1,8 @@
 'use strict';
+
+page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
+page('/books/:id', app.Book.initDetailPage);
+page('/books/new', app.Book.initAddBook);
+
+
+page();
