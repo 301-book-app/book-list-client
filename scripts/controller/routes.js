34d977fc3fb9bebd40b1ff0df57cc58269295da5
Beverly Pham <beverly.pham@gmail.com>
2018-02-28 12:55:23 -0800

    update routes

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -2,6 +2,7 @@
 
 page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/new', app.bookView.initAddBook);
+page('/books/aboutus', app.bookView.initAboutPage);
 page('/books/:id', ctx => getBookInfo(ctx, app.bookView.initDetailPage));
 
 page();
