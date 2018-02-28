f187fa635e70a648c6b27664b5b4495e12f214f8
Beverly Pham <beverly.pham@gmail.com>
2018-02-28 11:33:54 -0800

    move controller folder and fix routes

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,8 +1,8 @@
 'use strict';
 
 page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
-page('/books/:id', app.bookView.initDetailPage);
 page('/books/new', app.bookView.initAddBook);
+page('/books/:id', app.bookView.initDetailPage);
 
 
 page();
