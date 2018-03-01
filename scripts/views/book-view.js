49815b113e361c5e97848452f018e792c3cb11f0
Beverly Pham <beverly.pham@gmail.com>
2018-03-01 11:53:21 -0800

    added update and delete buttons on detail view

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -24,12 +24,31 @@ var app = app || {};
     $('.detail').empty();
     let template = Handlebars.compile($('#detail-template').html());
     $('#detail-view').append(template(ctxBook));
+    $('#update-form-view').hide();
+    $('#delete-button').on('click', (event) => {
+      module.Book.delete(event.target.attributes['data-id'].value);
+    });
+    $('#update-button').on('click', () => {
+      $('#update-form-view').show();
+      $('#update-form').on('submit', (event) => {
+        event.preventDefault();
+
+        let book = {
+          book_id: $('#update-form button').data('id'),
+          title: event.target.title.value,
+          author: event.target.author.value,
+          isbn: event.target.isbn.value,
+          image_url: event.target.image_url.value,
+          description: event.target.description.value,
+        };
+        module.Book.updateBook(book);
+      });
+    });
   };
 
   bookView.initAddBook = () => {
     $('.container').hide();
     $('#form-view').show();
-    $('#add-form').reset();
     $('#add-form').on('submit', event => {
       event.preventDefault();
 
